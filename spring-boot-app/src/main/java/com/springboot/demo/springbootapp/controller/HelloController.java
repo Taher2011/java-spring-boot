deb3b8a31b88e737ce9927645224cd562c3032f6
Taher <Taher@192.168.1.4>
2019-03-31 18:33:01 +0530

    SpringBoot

---
+++ spring-boot-app/src/main/java/com/springboot/demo/springbootapp/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.springboot.demo.springbootapp.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+	@Value("${developer.name}")
+	private String developerName;
+
+	@Value("${developer.company}")
+	private String developerCompany;
+
+	@GetMapping("/time")
+	public String getTime() {
+		return "Current time on server is " + LocalDateTime.now();
+	}
+
+	@GetMapping("/details")
+	public String getdetails() {
+		return "Developer Name is " + this.developerName + " and Company is " + this.developerCompany;
+	}
+
+}
