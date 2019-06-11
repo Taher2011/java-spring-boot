51448abbe8f831a9a61c700c380660bdf2284b94
Taher <Taher@192.168.1.4>
2019-06-11 06:17:28 +0530

    @RequestMapping

--- spring-boot-app/src/main/java/com/springboot/demo/springbootapp/controller/HelloController.java
+++ spring-boot-app/src/main/java/com/springboot/demo/springbootapp/controller/HelloController.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -23,6 +24,7 @@ public class HelloController {
 	}
 
 	@GetMapping("/details")
+	//@RequestMapping(value="/details", method=RequestMethod.GET)
 	public String getdetails() {
 		return "Developer Name is " + this.developerName + " and Company is " + this.developerCompany;
 	}
